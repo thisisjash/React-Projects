73600a1200c9fb56d063b1b3a46403255230241e
Jaswanth Surabattula <thisisjash619@gmail.com>
2022-01-02 18:05:27 +0530

    Listing Details and Added Clear Me Functionality

--- bday-reminder/src/App.jsx
+++ bday-reminder/src/App.jsx
@@ -1,9 +1,19 @@
 import React, { useState } from "react";
 import data from "./data";
-import Lisy from "./list";
+import List from "./list";
 
 const App = () => {
-  return <h2>Reminder Project Setup</h2>;
+  const [people, setPeople] = useState (data);
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} Birthdays Today</h3>
+        <List people = {people}/>
+        <button onClick={() => setPeople([])}>Clear All</button>
+      </section>
+    </main>
+  );
 };
 
 export default App;
