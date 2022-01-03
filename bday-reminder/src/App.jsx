ef9552b9157fb80b2be6555b51f8ee2ca778e8f2
Jaswanth Surabattula <thisisjash619@gmail.com>
2022-01-03 23:39:05 +0530

    Installed Firebase and Tweaked with UI

--- bday-reminder/src/App.jsx
+++ bday-reminder/src/App.jsx
@@ -3,13 +3,13 @@ import data from "./data";
 import List from "./list";
 
 const App = () => {
-  const [people, setPeople] = useState (data);
+  const [people, setPeople] = useState(data);
 
   return (
     <main>
       <section className="container">
         <h3>{people.length} Birthdays List</h3>
-        <List people = {people}/>
+        <List people={people} />
         <button onClick={() => setPeople([])}>Clear All</button>
       </section>
     </main>
--- emp-details-firebase/src/App.jsx
+++ emp-details-firebase/src/App.jsx
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
+import ScrollArea from "react-scrollbar/dist/no-css";
 import data from "./data";
 import List from "./list";
 
 const App = () => {
-  const [people, setPeople] = useState (data);
+  const [people, setPeople] = useState(data);
 
   return (
     <main>
       <section className="container">
-        <h3>{people.length} Birthdays Today</h3>
-        <List people = {people}/>
-        <button onClick={() => setPeople([])}>Clear All</button>
+        <section className="tool-bar">
+          <h3>Employee Details</h3>
+          <h4>Number of Employees - {people.length}</h4>
+          <button onClick={() => console.log("I AM CLICKED")}>Add New Employee</button>
+          <button onClick={() => setPeople([])}>Clear All</button>
+        </section>
+      </section>
+
+      <section className="container">
+        <List people={people} />
       </section>
     </main>
   );
